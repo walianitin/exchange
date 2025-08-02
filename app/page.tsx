c06c0c561f3babf970b788bd81ee613a58ed421d
walianitin <walianitin828@gmail.com>
2025-08-02 20:05:23 +0530

    added home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,15 @@
-"use client"
+"use client";
 
 
-import  Appbar  from "../components/ui/Appbar";
-
+import Landing from "@/components/ui/Landing";
 export default function Home() {
-// const router=useRouter();
-// const [loader ,setloader]=useState(true);
-const number =0;
-    // useEffect(()=>{
-    //     const timer= setTimeout(() => {
-    //            router.push("/market")
-    //     }, 5*1000); 
-            
-    // return ()=>clearTimeout(timer)
-    // })
   
-            return <div className=" bg-black min-h-screen p-4 tracking-widest ">    
-
-                <div className=" mb-10"></div>
-                <Appbar TrueButton={number as number}></Appbar>
-            </div>
+  return (
+            <>
+            <div className=" min-h-screen max-w-full bg-neutral-900">     
+            <Landing></Landing>
+              </div>
+            </>
+  
+  );
 }
